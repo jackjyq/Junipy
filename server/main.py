3b31b1d6c8b0a5c5bcb7d4982ecc49f445aaae17
seanyexx <sean.yex@gmail.com>
2018-05-09 23:02:00 +1000

    dict to array

--- server/main.py
+++ server/main.py
@@ -1,16 +1,21 @@
 import model
 from mongoengine import connect
+from collections import defaultdict
 from server_config import *
-
+import numpy as np
+from normalize_dict_to_array import *
 if __name__ == "__main__":
-    code_to_country, \
-    country_to_code, \
-    indicator_to_description = model.init_query_dict()
+	DB_URL = "mongodb://junipy:comp9321@ds014658.mlab.com:14658/junipy_deploy"
+	db_client = connect(host=DB_URL)
+	code_to_country, \
+	country_to_code, \
+	indicator_to_description = model.init_query_dict()
 
-    print(code_to_country.keys())
+	index_key_list=list(indicator_to_description.keys())
 
+	analysis=Analysis(code_to_country,index_key_list,DB_URL,2000,50)
 
-    # db_client = connect(host=DB_URL)
-    # print(model.query_country('USA')['1988'])
-
-    # db_client.close()
+	taken_country_list,\
+	taken_data=analysis.select_top_k_country()
+	
+	country_index_d=analysis.select_non_null(taken_country_list,taken_data)
