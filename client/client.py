67b041d759c139ff9830482e948f3b3515fa45b8
lmcmz <lmcmze@gmail.com>
2018-05-14 04:00:59 +1000

    Add country flags

--- client/client.py
+++ client/client.py
@@ -1,13 +1,20 @@
 from flask import Flask,jsonify, url_for, Response, render_template, make_response, redirect
 from flask_restful import reqparse, request
 import requests,html,urllib.parse
-import base64
+import base64, json
 
 app = Flask(__name__)
+flagList = []
 
+def loadCountryFlag():
+	with open('./static/data/country_flags.json') as json_data:
+		return json.load(json_data)
+		
 @app.route('/', methods=['POST','GET'])
 def lga():
-	return render_template('index.html'), 200
+	flagList = loadCountryFlag()
+	print(len(flagList))
+	return render_template('index.html', flagList=flagList), 200
 
 if __name__ == "__main__":
 	app.config['JSON_AS_ASCII'] = False
