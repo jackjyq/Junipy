ca20b28e954668cfebf223a7020af447c76c0b36
lmcmz <lmcmze@gmail.com>
2018-05-21 02:18:12 +1000

    Add detail page

--- client/client.py
+++ client/client.py
@@ -1,20 +1,28 @@
 from flask import Flask,jsonify, url_for, Response, render_template, make_response, redirect
 from flask_restful import reqparse, request
 import requests,html,urllib.parse
-import base64, json
+import base64, json, requests
 
 app = Flask(__name__)
 flagList = []
+
+apiBase = "http://127.0.0.1:5000"
 
 def loadCountryFlag():
 	with open('./static/data/country_flags.json') as json_data:
 		return json.load(json_data)
 		
 @app.route('/', methods=['POST','GET'])
-def lga():
+def home():
 	flagList = loadCountryFlag()
-	print(len(flagList))
-	return render_template('index.html', flagList=flagList), 200
+#	print(len(flagList))
+	return render_template('home.html', flagList=flagList), 200
+
+@app.route('/<country>', methods=['POST','GET'])
+def detail(country):
+	response = requests.get(apiBase+"/detail/CHN")
+	country = json.loads(response.text)
+	return render_template('detail.html', country=country), 200
 
 if __name__ == "__main__":
 	app.config['JSON_AS_ASCII'] = False
