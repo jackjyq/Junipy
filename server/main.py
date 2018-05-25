e472433969abfeb33f79140ced69e8a15bbc7d65
Joanna <joanna@JoannadeMacBook-Pro.local>
2018-05-25 21:17:46 +1000

    delete unuseble

--- server/main.py
+++ server/main.py
@@ -257,61 +257,6 @@ def get_flags():
 if __name__ == "__main__":
 	db_client = connect(host=DB_URL)
 	app.run()
-	data_dict = model.query(OVERVIEW, 'lastest_GDP')
-	data_remove = data_dict.copy()
-	for i in data_dict:
-		if data_dict[i] == None:
-			data_dict[i] = 0
-			data_remove.pop(i)
-		else:
-			shorts = global_codes[i]["short"]
-			data_remove[shorts] = data_remove.pop(i)
-			data_remove[shorts] = float(data_dict[i])
-	data_sort = sorted(data_remove.items(), key=lambda d:d[0])
-	min_dict = sorted(data_remove.items(),key = lambda d:d[1],reverse = True)[-1][1]
-	divide = int(min_dict)
-	a = len(str(divide))
-	print(a)
-	raw_data = model.query(INDICATOR, 'USA')
-	data_detail = {}
-	for n in global_indicator:
-		data_detail[global_indicator[n]+'History'] = []
-	for i in range(1990,2017):
-		gdp = float(raw_data[str(i)]['GDP_total'])
-		for j in raw_data[str(i)]:
-			if j == 'GDP_agriculture' or j == 'GDP_industry' or j == 'GDP_service':
-				if not raw_data[str(i)][j] == None:
-					da = float(raw_data[str(i)][j])
-					va = ( da * gdp)/100
-				else:
-					va = None
-				da = {'year':i ,'value':va}
-			else:
-				da = {'year':i,'value':raw_data[str(i)][j]}
-			key = global_indicator[j] + 'History'
-			data_detail[key].append(da)
-	print(data_detail)
-	for i in data_detail:
-		if 'History' in i:
-			allva = []
-			for k in range(len(data_detail[i])):
-				if not data_detail[i][k]['value'] == None:
-					notst = float(data_detail[i][k]['value'])
-					allva.append(notst)
-				if not len(allva) == 0:
-					min_da = (sorted(allva))[0]
-					divide = int(min_da)
-					a = len(str(divide))
-					a = 10 ** a
-			for k in range(len(data_detail[i])):
-				if not len(allva) == 0:
-					if not data_detail[i][k]['value'] == None:
-						notst = float(data_detail[i][k]['value'])
-						ll = notst / a
-						data_detail[i][k]['value'] = '%.2f'% ll
-	print(data_detail)
-
-	#app.run()
 	# data_detail = {}	
 	# raw_data = model.query(INDICATOR, 'USA')
 	# #print(raw_data)
